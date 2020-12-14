45bb58b26dbae48b7aa8c35d9b33dd9514287790
SamhitaG <72413840+SamhitaG@users.noreply.github.com>
2020-12-13 19:47:43 -0600

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,32 +1,33 @@
-var thickness ,wall;
-var speed,weight,bullet,def;
+var thickness ,wall ;
+var speed,weight,bullet,def, height;
 
 function setup() {
-  console.log(" why is this not working -setup");
+  console.log(" line 5");
   speed=random(223,321);
   weight=random(30,52);
-  thickness=random(22,83);
+  height = 50;
+ thickness=random(22,83);
   createCanvas(800,400);
-  createSprite(400, 200, 50, 50);
-  bullet=createSprite(50,200,50,50);
-  wall=createSprite(1200,200,thickness,height/2);
+  bullet = createSprite(50,200,50,50);
+  wall = createSprite(400,200,10,50);
   bullet.velocityX=speed/10;
   wall.shapeColor=color(80,80,80);
   bullet.shapeColor="blue";
-  console.log(" why is this not working -setup");
+  console.log("line 16 ");
+ //wall= createSprite(400, 200, 10, 50);
 }
 
 function draw() {
-  console.log(" why is this not working - draw");
+  console.log(" line 20");
   background(255,255,255);
  if(wall.x-bullet.x < (bullet.width+wall.width)/2){
    def = 0.5*weight*speed*speed;}
   if(def<100){bullet.shapeColor="green";} 
   
-  if(def>100 && def<180 ||def==100 ){bullet.shapeColor="blue";} 
+  if(def>100 && def<180 ||def==100 ){bullet.shapeColor="pink";} 
   
   if(def>179){bullet.shapeColor="red";} 
-  hasCollided();
+if(wall.isTouching(bullet)){bullet.velocityX=0}
   if(hasCollided(bullet,wall)){
 bullet,velocityX=0;
 var damage=0.5*weight*speed*speed/(thickness*thickness*thickness);
@@ -38,7 +39,7 @@ if(damage<10){
 }
 
   }
-   
+
   drawSprites();
 }
 function hasCollided(lbullet,lwall)
